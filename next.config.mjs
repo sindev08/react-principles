35f33817b5176ce27504939832b8c9547e9ad700
Singgih Budi Purnadi <57170834+sindev08@users.noreply.github.com>
2026-04-06 14:48:56 +0700

    Revert "config: add reactStrictMode and images.remotePatterns baseline"
    This reverts commit 67b2bf9e6395228799a3f2eba526cecf3913dc12.

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
